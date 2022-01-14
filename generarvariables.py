fc09fbf9340dc90e65921cfee27674b8a3c730d9
jpfloww <jean-pierre.rojas@sansano.usm.cl>
2022-01-14 12:04:24 -0500

    Se generó una función para que la configuracion del bot de posiciones de pixeles y respectivos colores sea lo más fácil posible. Ahora es capaz de detectar si hay una ventana de combate para cerrarla, y si se encuentra en combate

--- generarvariables.py
+++ generarvariables.py
@@ -6,16 +6,29 @@ import mouse
 #lista=[]
 archivo=open('constantes.txt','w')
 contador=0
-print('Mueva el puntero del ratón al icono de mis hechizos en la parte amarilla de la varita.')
+contador2=0
+print('Mueva el puntero del ratón al icono de mis hechizos en la parte amarilla de la varita. Luego presione Ctrl')
 while True:
-
+    contador2=0
     if keyboard.is_pressed('ctrl'):
-        if contador==0:
+        if contador==0 and contador2==0:
             #lista.append(['spellicon', pyautogui.position(),pyautogui.pixel(pyautogui.position().x, pyautogui.position().y) ]) # nombre, (x,y) , RGB(255,200,200)
             archivo.write('spellicon; '+str(pyautogui.position().x)+', '+str(pyautogui.position().y)+'; '+str(pyautogui.pixel(pyautogui.position().x, pyautogui.position().y)))
             archivo.write('\n')
             print('Añadido correctamente: '+'color: '+str(pyautogui.pixel(pyautogui.position().x, pyautogui.position().y) ))
             contador=1
+            contador2=1
+            print('Mueva el puntero hacia el marco lateral negro de la ventana de termino de combate. Luego presione Ctrl')
+            time.sleep(0.5)
+
+        if contador==1 and contador2==0:
+
+            archivo.write('endfightwindow; '+str(pyautogui.position().x)+', '+str(pyautogui.position().y)+'; '+str(pyautogui.pixel(pyautogui.position().x, pyautogui.position().y)))
+            archivo.write('\n')
+            print(' Añadido correctamente')
+            contador=2
+            contador2=1
+            time.sleep(0.5)
 
 
 
--- procedimiento.py
+++ procedimiento.py
@@ -1,12 +1,33 @@
-import pyautogui
+import pyautogui, time
+import keyboard
 archivo=open('constantes.txt','r')
 lineas=archivo.readlines()
 archivo.close()
 #Leo primera linea que tiene que ver con estaencombate si no esta la varita
 spelliconpos=(lineas[0].split(";")[1].split(',')[0], lineas[0].split(";")[1].split(',')[1]) # spelliconpos[0] es x. analogo y
-spelliconcolor=lineas[0].split(";")[2]
-print(spelliconcolor)
-print(pyautogui.pixel(int(spelliconpos[0]),int(spelliconpos[1])))
-print(   pyautogui.pixelMatchesColor(int(spelliconpos[0]),int(spelliconpos[1]), spelliconcolor ) )
+spelliconcolor=tuple(map(int,lineas[0].split(";")[2].replace('(','').replace(')','').split(',')))
+
+#Leo segunda linea que tiene que ver si esta la ventana de cierre de combate
+endfightwindowpos=(lineas[1].split(";")[1].split(',')[0], lineas[1].split(";")[1].split(',')[1])
+endfightwindowcolor=tuple(map(int,lineas[1].split(";")[2].replace('(','').replace(')','').split(',')))
+print(endfightwindowpos)
+print(pyautogui.pixel(int(endfightwindowpos[0]),int(endfightwindowpos[1])))
+print(pyautogui.pixelMatchesColor(int(endfightwindowpos[0]), int(endfightwindowpos[1]), endfightwindowcolor, tolerance=30))
 def estaencombate():
-    print('estoyencombate')
+    if pyautogui.pixelMatchesColor(int(spelliconpos[0]),int(spelliconpos[1]),  spelliconcolor, tolerance=40):
+        print('sale verdadero, no esta en combate')
+        return False
+    else:
+        print('sale falso, si esta en combate')
+        return True
+def cerrarventanacombate():
+    if pyautogui.pixelMatchesColor(int(endfightwindowpos[0]), int(endfightwindowpos[1]), endfightwindowcolor, tolerance=3):
+        print('Se encuentra ventana, se cierra')
+        keyboard.press('esc')
+        keyboard.release('esc')
+
+while True:
+    if estaencombate():
+        print('en combate')
+    cerrarventanacombate()
+    time.sleep(4)
