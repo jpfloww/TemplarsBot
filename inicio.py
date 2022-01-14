c987153d7794aadb9c346af95bf23d9708d0bc3a
jpfloww <jean-pierre.rojas@sansano.usm.cl>
2022-01-13 21:28:53 -0500

    Implementando libreria Pynput pero aun no funciona. El error es que reconoce las pulsaciones de mouse pero no las pulsaciones de teclado, todo esto tiene que ver con Listener()

--- inicio.py
+++ inicio.py
@@ -1,1 +1,24 @@
-print('hola')
+from pynput.mouse import Listener
+from pynput import keyboard as kb
+lista=[]
+def on_click(x, y, button, pressed):
+    archivo = open("posiciones.txt", "a")
+    if pressed:
+        print('Se añade la posicion: '+str(x)+', '+str(y)+' ).')
+        lista.append( (x,y))
+        print(lista)
+
+def suelta(tecla):
+    print('se suelta la tecla'+str(tecla))
+    if tecla== kb.KeyCode.from_char('q'):
+        return False
+
+print('Bienvenido, 1: Agregar nueva posicion en un mapa. 2: Usar posicion existente')
+opcion=input('Ingrese opción: ')
+if int(opcion)==1:
+    with Listener(
+            on_click=on_click , suelta=suelta) as listener:
+        listener.join()
+
+elif int(opcion)==2:
+    print("desarrollandose")
---
+++ inicio2.py
@@ -0,0 +1,5 @@
+import mouse, time
+while True:
+    lambda = 'no'
+    mouse.on_click(: print("Left buttonn is clicked"))
+    time.sleep(0.5)
