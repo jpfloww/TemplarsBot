7c27b73c8292580138ebe79b5c081c91c98b4356
jpfloww <jean-pierre.rojas@sansano.usm.cl>
2022-01-14 08:57:33 -0500

    Se genera función para generar las coordenadas de los recursos facilmente, aún falta implementar la acción de recolectar que trabaja con los datos de la función generada.

---
+++ generarcoordenadas.py
@@ -0,0 +1,35 @@
+import pyautogui
+import keyboard
+import time
+import mouse
+lista=[]
+zona=input('Ingrese Zona: ')
+recursos=input('Ingrese cantidad de recursos': )
+posicion=input('Ingrese posicion: ')
+listadetalles=[zona,recursos,posicion]
+listacolores=[]
+while True:
+
+    if keyboard.is_pressed('ctrl'):
+        break
+    if mouse.is_pressed(button='left'):
+        print(pyautogui.pixel(pyautogui.position().x, pyautogui.position().y))
+        lista.append(pyautogui.position())
+        listacolores.append(pyautogui.pixel(pyautogui.position().x, pyautogui.position().y))
+        time.sleep(0.1)
+    time.sleep(0.1)
+print(lista)
+archivo=open('posiciones.txt','a')
+archivo.write(str(lista))
+archivo.write('\n')
+archivo.close()
+
+detalle=open('detalles.txt','a')
+detalle.write(str(listadetalles))
+detalle.write('\n')
+detalle.close()
+
+archivocolores=open('colores.txt','a')
+archivocolores.write(str(listacolores))
+archivocolores.write('\n')
+archivocolores.close()
--- inicio2.py
+++
@@ -1,5 +0,0 @@
-import mouse, time
-while True:
-    lambda = 'no'
-    mouse.on_click(: print("Left buttonn is clicked"))
-    time.sleep(0.5)
